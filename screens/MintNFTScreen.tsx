ea5953c45a889c341f25734b0879828c57ea407c
munirrani <munirmohamadrani@gmail.com>
2022-10-09 19:13:10 +0800

    Update MintNFTScreen

--- screens/MintNFTScreen.tsx
+++ screens/MintNFTScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { StyleSheet, Image, Dimensions, ScrollView, Alert } from 'react-native';
+import { StyleSheet, Image, Dimensions, ScrollView, Alert, TouchableOpacity, ImageBackground, Text } from 'react-native';
 import { View } from '../components/Themed';
 import { setDoc, doc, Timestamp, updateDoc } from 'firebase/firestore';
 import { db } from '../db-config';
@@ -11,42 +11,23 @@ import WalletConnectProvider from '@walletconnect/web3-provider';
 import { useWalletConnect } from '@walletconnect/react-native-dapp';
 import StatusMessage from '../components/StatusMessage';
 import { INFURA_ID, NFTPORT_AUTH} from '@env';
+import * as ImagePicker from 'expo-image-picker';
 
 const NFTSmartContractAddress = "0xc9a253097212a55a66e5667e2f4ba4284e5890de"
 const NFTSmartContractABI = require('../contracts/abi/PhotoToken.json')
 
-const date = new Date()
-const timestampDefault = Timestamp.fromDate(date)
-
-const firebase_data = {
-  "marketplace_metadata": {
-    "isListed": false,
-    "listing_date": timestampDefault,
-	"listing_price": "0.0"
-  },
-  "nft_metadata": {
-    "description": "",
-    "image_name": "",
-    "ipfs_image_url": "",
-    "ipfs_metadata_uri": "",
-    "minted_date": timestampDefault,
-    "nft_address": NFTSmartContractAddress,
-    "owner_address": "",
-    "token_id": 0,
-    "mint_transaction_hash": "",
-    "listing_transaction_hash": "",
-	"external_url": "",
-  },
-}
-
 export default function MintNFTScreen() {
+
+	// Wallet
+	const [isWalletConnected, setIsWalletConnected] = useState(false);
+	const [isStartingTransaction, setIsStartingTransaction] = useState(false);
+	const [isSubmittingTransaction, setIsSubmittingTransaction] = useState(false);
 
     const [nameText, onChangeNameText] = useState<string>('');
     const [descriptionText, onChangeDescriptionText] = useState<string>('');
     const [externalURLText, onChangeExternalURLText] = useState<string>('');
-    const [fileResponse, setFileResponse] = useState([]);
+    const [imageResponse, setImageResponse] = useState(null);
 
-    const [buttonPressed, setButtonPressed] = useState(false);
 	const [doneUploadImage, setDoneUploadImage] = useState(false);
 	const [doneUploadMetadata, setDoneUploadMetadata] = useState(false);
 	const [doneMinting, setDoneMinting] = useState(false);
@@ -55,33 +36,37 @@ export default function MintNFTScreen() {
 
 	var ipfsImageURL;
 	var ipfsMetadataURL;
-	
 	var tokenId;
 	var mintTxHash;
 	var blockNumber;
- 
-	useEffect(()=> {
-	}, [doneUploadImage, doneUploadMetadata, doneMinting])
 
-    const handleDocumentSelection = useCallback(async () => {
-      try {
-        const response = await DocumentPicker.getDocumentAsync({
-          type: "image/*",
-          multiple: false,
-        });
-        setFileResponse(response);
-      } catch (err) {
-        console.warn(err);
-      }
-    }, []);
+	useEffect(()=> {
+		setIsWalletConnected(walletConnector.connected)
+	}, [])
+
+
+	const handleImageSelection = async() => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			quality: 1,
+		});
+		
+		if (!result.cancelled) {
+			setImageResponse(result);
+		}
+	}
 
     const uploadImageToIPFS = async() => {
       console.log("Uploading image to IPFS...");
       const form = new FormData();
+
+	  const filePathParts = imageResponse.uri.split("\/")
+	  const fileName = filePathParts[filePathParts.length - 1]
+
       form.append("file", {
-        uri: fileResponse.uri,
-        type: "image/jpeg",
-        name: fileResponse.name
+        uri: imageResponse.uri,
+        type: fileName.includes(".jpg") ? "image/jpeg" : "image/png",
+		name: fileName
       });
       const options = {
         method: 'POST',
@@ -92,14 +77,26 @@ export default function MintNFTScreen() {
       };
       options.body = form
 
-      await fetch('https://api.nftport.xyz/v0/files', options)
-        .then(response => response.json())
-        .then(response => {
-          console.log(response);
-		  ipfsImageURL = response.ipfs_url;
-		  setDoneUploadImage(true);
-        })
-        .catch(err => console.error(err));
+	  try {
+			const request = await fetch('https://api.nftport.xyz/v0/files', options)
+			const data = await request.json()
+			console.log(data)
+			ipfsImageURL = data.ipfs_url
+			setDoneUploadImage(true)
+	  } catch (error) {
+		Alert.alert("Error", error.toString(),
+		[
+			{
+				text: "Ok",
+				style: "default",
+			},
+			],
+			{
+				cancelable: true,
+			}
+		);
+		console.error(error)
+	  }
     }
 
     const uploadMetadataToIPFS = async() => {
@@ -112,7 +109,7 @@ export default function MintNFTScreen() {
       }
 	  if (externalURLText) metadata.external_url = externalURLText;
 
-      const options2 = {
+      const options = {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -121,14 +118,26 @@ export default function MintNFTScreen() {
         body: JSON.stringify(metadata)
       };
 
-      await fetch('https://api.nftport.xyz/v0/metadata', options2)
-        .then(response => response.json())
-        .then(response => {
-          console.log(response);
-		  ipfsMetadataURL = response.metadata_uri
-		  setDoneUploadMetadata(true)
-        })
-        .catch(err => console.error(err));
+		try {
+			const request = await fetch('https://api.nftport.xyz/v0/metadata', options)
+			const data = await request.json()
+			console.log(data)
+			ipfsMetadataURL = data.metadata_uri
+			setDoneUploadMetadata(true)
+		} catch (error) {
+			Alert.alert("Error", error.toString(),
+			[
+				{
+					text: "Ok",
+					style: "default",
+				},
+				],
+				{
+					cancelable: true,
+				}
+			);
+			console.error(error)
+		}
     }
 
     const mintNFT = async(gasPrice: BigNumber, signer: any) => {
@@ -138,17 +147,20 @@ export default function MintNFTScreen() {
         const estimatedGasLimit = await NFTContract.estimateGas.mintNFT(decodeURI(encodeURI(ipfsMetadataURL)))
         
         await NFTContract.mintNFT(decodeURI(encodeURI(ipfsMetadataURL)), {
-          gasPrice: gasPrice,
-          gasLimit: estimatedGasLimit
+			gasPrice: gasPrice,
+			gasLimit: estimatedGasLimit
         })
-		.then((tx:any) => tx.wait())
+		.then((tx:any) => {
+			setIsSubmittingTransaction(true)
+			return tx.wait()
+		})
         .then((result:any) => {
-          console.log("Minting TX Result");
-		  console.log(result);
-		  mintTxHash = result.transactionHash;
-		  tokenId = parseInt(result.events[0].topics[3]);
-		  blockNumber = result.blockNumber;
-		  setDoneMinting(true)
+			console.log("Minting TX Result");
+			console.log(result);
+			tokenId = parseInt(result.events[0].topics[3])
+			mintTxHash = result.transactionHash
+			blockNumber = result.blockNumber
+			setDoneMinting(true)
         })
       } catch (error) {
 		Alert.alert("Error", error.toString(),
@@ -162,47 +174,46 @@ export default function MintNFTScreen() {
     }
 
     const putMetadataToFirebase = async(web3provider: any) => {
-      console.log("Done minting, now putting into Firebase...");
-	  console.log("Getting block number timestamp...")
-	  const request = await web3provider.getBlock(blockNumber);
-	  const time = Timestamp.fromDate(new Date(request.timestamp * 1000));
+		console.log("Done minting, now putting into Firebase...");
+		
+		const request = await web3provider.getBlock(blockNumber);
+		// Get block timestamp
+		const time = Timestamp.fromDate(new Date(request.timestamp * 1000));
+		const address = walletConnector.accounts[0]
 
-      const firebase_data2:any = JSON.parse(JSON.stringify(firebase_data, null, 2));
-      firebase_data2.nft_metadata = {
-        image_name: nameText,
-        description: descriptionText,
-        ipfs_image_url: ipfsImageURL,
-        ipfs_metadata_uri: ipfsMetadataURL,
-        minted_date:  time,
-        owner_address: walletConnector.accounts[0],
-        token_id: tokenId,
-        mint_transaction_hash: mintTxHash,
-      }
-	  if (externalURLText) firebase_data2.nft_metadata.external_url = externalURLText;
+		const firebase_data = {
+			marketplace_metadata: {
+				isListed: false,
+			},
+			nft_metadata: {
+				current_owner_address: address,
+				description: descriptionText,
+				external_url: externalURLText ? externalURLText : "",
+				image_name: nameText,
+				ipfs_image_url: ipfsImageURL,
+				ipfs_metadata_uri: ipfsMetadataURL,
+				mint_transaction_hash: mintTxHash,
+				minted_date: time,
+				original_owner_address: address,
+				token_id: tokenId,
+			},
+		}
 
-      await setDoc(doc(db, "NFT", "NFT-" + tokenId), firebase_data2)
-    }
-
-    const putMetadataToFirebase2 = async() => {
-      console.log("Rebuilding database");
-
-      const firebase_data2:any = JSON.parse(JSON.stringify(CONTENT[0], null, 2));
-
-      await setDoc(doc(db, "NFT", "NFT-14"), firebase_data2)
-    }
-
-    const putMetadataToFirebase3 = async() => {
-      console.log("Rebuilding database");
-
-      const firebase_data2:any = JSON.parse(JSON.stringify(CONTENT[0], null, 2));
-
-      await updateDoc(doc(db, "NFT", "NFT-14"), {
-		["marketplace_metadata.listing_price"]: "",
-	  })
+		await setDoc(doc(db, "NFT", "NFT-" + tokenId), firebase_data)
     }
 
 	const doChecks = () => {
-		if (!walletConnector.connected) {
+		function isValidHttpUrl(string: string) {
+			let url;
+			try {
+			  url = new URL(string);
+			} catch (_) {
+			  return false;  
+			}
+			return true
+		}
+
+		if (!isWalletConnected) {
 			Alert.alert("Error", "Wallet is not connected.",
 			[
 				{
@@ -216,7 +227,7 @@ export default function MintNFTScreen() {
 			);		
 			return false
 		} 
-		if (fileResponse.type !== "success") {
+		if (!imageResponse) {
 			Alert.alert("Error","Image not uploaded",
 			[
 				{
@@ -245,11 +256,25 @@ export default function MintNFTScreen() {
 			);
 			return false
 		}
+
+		if (externalURLText && !isValidHttpUrl(externalURLText)) {
+			Alert.alert("Error", "In valid URL format",
+			[
+				{
+					text: "Ok",
+					style: "default",
+				},
+				],
+				{
+					cancelable: true,
+				}
+			);
+			return false
+		}
 		return true
 	}
 
-    const doIt = async() => {
-      
+    const startMinting = async() => {
 		const provider = new WalletConnectProvider({
 			infuraId: INFURA_ID,
 			connector: walletConnector
@@ -273,57 +298,60 @@ export default function MintNFTScreen() {
 
 		const gasPrice = await web3Provider.getGasPrice()
 			
-		uploadImageToIPFS()
-		.then(() => uploadMetadataToIPFS())
-		.then(() => mintNFT(gasPrice, signer))
-		.then(() => putMetadataToFirebase(web3Provider))
+		try {
+			await uploadImageToIPFS()
+			await uploadMetadataToIPFS()
+			await mintNFT(gasPrice, signer)
+			await putMetadataToFirebase(web3Provider)
+		} catch (error) {
+			Alert.alert("Error", error.toString(),
+			[
+				{ text: "Ok", style: "default", },
+			],
+				{ cancelable: true, }
+			);
+			console.log(error)
+		}
     }
 
-	const callContractTest = async() => {
-		const provider = new WalletConnectProvider({
-			infuraId: INFURA_ID,
-			connector: walletConnector
-		});
-		await provider.enable()
-		
-		const web3Provider = new providers.Web3Provider(provider);
-		const signer = web3Provider.getSigner();
-
-		provider.on("accountsChanged", (accounts: string[]) => {
-			console.log(accounts);
-		});
-
-		provider.on("chainChanged", (chainId: number) => {
-			console.log(chainId);
-		});
-
-		provider.on("disconnect", (code: number, reason: string) => {
-			console.log(code, reason);
-		});
-
-		const NFTContract = new Contract(NFTSmartContractAddress, NFTSmartContractABI, signer);
-		const call = await NFTContract.tokenURI(0)
-		console.log(call)
+	const clearScreen = () => {
+		// If done minting, give option to clear screen
+		setIsStartingTransaction(false)
+		setIsSubmittingTransaction(false)
+		onChangeNameText('')
+		onChangeDescriptionText('')
+		onChangeExternalURLText('')
+		setImageResponse(null)
+		setDoneUploadImage(false)
+		setDoneUploadMetadata(false)
+		setDoneMinting(false)
 	}
-
-    // TODO: Do an "X" button to discard images and clear selection.
 
     return (
       <ScrollView style={{backgroundColor: "#ffffff"}}>
         <View style={styles.container}>
           <View style={{ margin:10 }}>
-            <View style={{alignItems: 'center', marginVertical: 10}}>
-              {fileResponse.type === "success" && (<Image 
-                source={{uri: fileResponse.uri}}
-                style={{
-                  height: 200,
-                  width: Dimensions.get('window').width - 30,
-                  marginBottom: 15,
-                }}
-                resizeMode="contain"
-              />)}
+            <View style={{alignItems: 'center'}}>
+              {imageResponse && (
+				<ImageBackground 
+					source={{uri: imageResponse.uri}}
+					style={{
+						marginBottom: 15,
+						height: 200,
+						width: '100%',
+						backgroundColor: "#f0f0f0"
+					}}
+					resizeMode="contain">
+					<TouchableOpacity style={{ position: 'absolute', top: 3, right: 3}} onPress={() => setImageResponse(null)}>
+						<Image 
+							source={require('../assets/images/x-icon.png')} 
+							style={{width: 25, height: 25, opacity: .4,}}
+						/>
+					</TouchableOpacity>
+				</ImageBackground>
+              )}
               <Button 
-                  onPress={handleDocumentSelection}
+                  onPress={handleImageSelection}
                   title={"✨ Upload Image ✨"}
               />
             </View>
@@ -356,6 +384,7 @@ export default function MintNFTScreen() {
 					}}
 					placeholder="Description"
 					multiline={true}
+					enableScrollToCaret
 				/>
 
 				<AutoGrowingTextInput 
@@ -374,23 +403,17 @@ export default function MintNFTScreen() {
 				/>
             </View>
             <View style={{alignItems: 'center', marginVertical: 10}}>
-              <Button 
-                title="Mint it!"
-                style={{
-                  backgroundColor: "#62f062",
-                }}
-                // onPress={() => {
-				// 	const check = doChecks()
-				// 	setButtonPressed(check)
-				// 	check && doIt()
-				// }}
-                onPress={() => {
-					putMetadataToFirebase3()
-				}}
-              />
-            </View>
+				<TouchableOpacity style={{padding: 10, borderRadius: 4, backgroundColor: 'green',}}
+					onPress={() => {
+						const check = doChecks()
+						setIsStartingTransaction(check)
+						check && startMinting()
+					}}>
+					<Text style={{color: 'white'}}>Mint it!</Text>
+				</TouchableOpacity>
+			</View>
 			<View style={{marginLeft: 10}}>
-				{ buttonPressed && 
+				{ isStartingTransaction && 
 					<StatusMessage content="Starting..." />
 				}
 				{ doneUploadImage && 
@@ -399,13 +422,21 @@ export default function MintNFTScreen() {
 				{ doneUploadMetadata && 
 					<StatusMessage content="Uploaded metadata to IPFS" />
 				}
+				{ isSubmittingTransaction && 
+					<StatusMessage content="Submitting transaction. Waiting for 1 confirmation..." />
+				}
 				{ doneMinting && 
 					<StatusMessage
-						content="Minted into NFT" 
+						content="NFT Minted!" 
 						txHash={mintTxHash}
 					/>
 				}
 			</View>
+			{ doneMinting && 
+				<TouchableOpacity style={{marginVertical: 20, alignItems: 'center', justifyContent: 'center'}} onPress={clearScreen}>
+					<Text style={{color: '#4989ad', fontSize: 15,}}>Clear screen</Text>
+				</TouchableOpacity>
+			}
           </View>
         </View>
       </ScrollView>
